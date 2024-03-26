11fe6831e078787a96d0aecf0757e6358f826aad
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-26 17:42:50 +0100

    edit ProductModal as a component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,34 @@
 <script>
 import ProductCard from './ProductCard.vue';
-// import { products } from '../data'
+// import { products } from '../data';
 // import axios from 'axios';
-import { store } from '../store'
+import { store } from '../store';
 import axios from 'axios';
+import ProductModal from './ProductModal.vue';
 
 export default {
     name: 'AppMain',
     components: {
         ProductCard,
+        ProductModal,
     },
     data() {
         return {
             // products: []
             store,
-            show: false,
-            modalProduct: ''
+            showModal: false,
+            modalProduct: {},
         }
     },
     methods: {
         showProductModal(id) {
-            this.show = !this.show;
-            console.log(id);
+            this.showModal = !this.showModal;
+            // console.log(id);
             axios.get(`http://localhost:3000/products/${id}`).then(response => {
-                console.log(response.data);
+                // console.log(response.data);
                 this.modalProduct = response.data;
             })
-        }
+        },
     },
     mounted() {
         /* axios.get('http://localhost:3000/products').then(response => {
@@ -41,52 +43,17 @@ export default {
 <template>
     <main id="site_main">
         <div class="container">
-
             <ProductCard :id="product.id" :frontImage="product.frontImage" :backImage="product.backImage"
                 :brand="product.brand" :name="product.name" :price="product.price"
                 :isInFavourites="product.isInFavourites" :badges="product.badges" v-for="product in store.products"
                 @showProduct="showProductModal" />
-
         </div>
-        <div class="modal" v-if="show === true">
-            <div class="product-modal-card">
-                <div class="modal-close" @click="show = !show">X</div>
-                <div class="product-name">{{ modalProduct.name }}</div>
-            </div>
 
-        </div>
+        <ProductModal :show="this.showModal" :frontImage="modalProduct.frontImage" :backImage="modalProduct.backImage"
+            :brand="modalProduct.brand" :name="modalProduct.name" :price="modalProduct.price"
+            :isInFavourites="modalProduct.isInFavourites" :badges="modalProduct.badges" />
+
     </main>
 </template>
 
-<style>
-.modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 15;
-    background-color: rgba(0, 0, 0, 0.7);
-    width: 100vw;
-    height: 100vh;
-
-    .product-modal-card {
-        width: 400px;
-        height: 400px;
-        background-color: darkslateblue;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-
-        .modal-close {
-            font-size: 3rem;
-            color: orangered;
-            background-color: palevioletred;
-            width: fit-content;
-        }
-
-        .product-name {
-            color: white;
-        }
-    }
-}
-</style>
+<style></style>
---
+++ src/components/ProductModal.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: 'ProductModal',
+    props: {
+        show: Boolean,
+        frontImage: String,
+        backImage: String,
+        brand: String,
+        name: String,
+        price: Number,
+        isInFavourites: Boolean,
+        badges: Array,
+    }
+}
+</script>
+
+<template>
+    <div class="modal" v-if="show === true">
+        <div class="product-modal-card">
+            <div class="modal-left">
+                <div class="thumb">
+                    <img class="thumb front-image" :src="frontImage" alt="">
+                    <img class="thumb back-image" :src="backImage" alt="">
+                </div>
+                <img class="active-image" src="" alt="">
+            </div>
+            <div class="modal-right">
+                <div class="modal-close" @click="show = !show">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                        <!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License -
+                            https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                    </svg>
+                </div>
+                <div class="product-brand">{{ brand }}</div>
+                <div class="product-name">{{ name }}</div>
+                <div class="susteinability"></div>
+                <div class="price">{{ price }} &euro;</div>
+                <div class="discount"></div>
+                <div class="final-price"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../assets/partials/ProductModal'
+</style>
